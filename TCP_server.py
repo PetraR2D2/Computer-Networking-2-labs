7e3dc4b535433524bbe875dedda1f6e6a265ff93
PetraR2D2 <petrar2d2@gmail.com>
2017-10-14 22:15:22 +0200

    Update TCP_server.py

--- TCP_server.py
+++ TCP_server.py
@@ -1,25 +1,31 @@
-from datetime import datetime   # Used to fetch the time as a server's response to client's request
+from datetime import datetime
 import socket
 
-HOST, PORT = '', 8888   # HOST = '' means the server is listening on all networking interfaces
+HOST, PORT = '', 8888   # "HOST = ''" means the server is listening on all networking interfaces.
 
-listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# The creation of the welcoming socket, and it's listening.
+listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   
 listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 listen_socket.bind((HOST, PORT))
 listen_socket.listen(1)
 print 'Serving HTTP on port %s ...' % PORT
+
 while True:
-    client_connection, client_address = listen_socket.accept()
+    client_connection, client_address = listen_socket.accept()  # TCP connection with a client established.
     request = client_connection.recv(1024)
     print request
 
-    if request == "SIMPLE TIME":    
-        http_response = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    # Now, here am I a little confused.
+    # "if" responds to a simple "SIMPLE TIME" request from the console.
+    # "else" response is designed for a "browser" (HTTP) request, but it can be used for any client with a 
+    # MESSAGE != "SIMPLE TIME", that is - where "if" condition is not satisfied.
+    if request == "SIMPLE TIME":
+        response = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     else:
-        http_response = """\
+        response = """\
 HTTP/1.1 200 OK
 
-%s""" % datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+%s u browseru""" % datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-    client_connection.sendall(http_response)
-    client_connection.close()
+    client_connection.sendall(response)         # HTTP response sent.
+    client_connection.close()                   # TCP connection closed.
